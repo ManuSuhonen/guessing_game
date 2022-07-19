0c1e2486800f6c91f002f0a56f32c2c57b49d0fd
Manu Suhonen <maun.suhonen@gmail.com>
2022-07-20 00:47:19 +0300

    add range for rand num generator

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use rand::*;
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -13,8 +17,4 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
-
-    let rand = random::<u8>();
-
-    println!("rand = {rand}");
 }
